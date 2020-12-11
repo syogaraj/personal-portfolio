9e8214d0644001129b1aea13d5b02ec7573972c2
Yogaraj Sivaprakasam <yoga30696@gmail.com>
2020-12-11 15:40:43 +0530

    removed template code generated by cli

--- src/App.vue
+++ src/App.vue
@@ -38,19 +38,16 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
   data: () => ({
