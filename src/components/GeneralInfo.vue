7dcf2bad17c1791fa8b9499e0ddb581082536a14
Yogaraj Sivaprakasam <yoga30696@gmail.com>
2021-02-26 23:04:21 +0530

    general info update

--- src/components/GeneralInfo.vue
+++ src/components/GeneralInfo.vue
@@ -2,26 +2,25 @@
 	<v-container>
 		<section>
 			<div id="my_avatar">
-				<img src="../assets/images/self/me.png" alt="Yogaraj.S" />
+				<img src="../assets/images/self/me.png" class="tw-border-8 tw-rounded-full tw-shadow-2xl" alt="Yogaraj.S" />
 			</div>
 			<div id="my_name">
 				<h1>Yogaraj Sivaprakasam</h1>
 			</div>
 			<v-divider light></v-divider>
 			<div id="my_designation">
-				<h3>Full Stack/Python Developer @Zoho</h3>
+				<h3>Full Stack/Python Developer</h3>
 			</div>
 			<div id="my_location">
 				<img src="../assets/images/social_media/location-pin.svg" alt="Location" />
 				<h5>Puducherry, India</h5>
 			</div>
-			<div id="my_social_media">
-				<div v-for="item in social_links" :key="item.id">
+			<div id="my_social_media" class="tw-hidden md:tw-flex md:tw-justify-between md:tw-w-9/12 lg:tw-w-6/12">
+				<div v-for="item in social_links" :key="item.id" @click="openLink(item)">
 					<v-tooltip bottom>
 						<template v-slot:activator="{ on, attrs }">
 							<v-img
 								:src="item.src"
-								@click="openLink(item)"
 								width="60"
 								height="60"
 								v-on="on"
@@ -33,7 +32,7 @@
 				</div>
 			</div>
 			<div id="get_started">
-				<v-btn depressed rounded large>Let's get started!</v-btn>
+				<v-btn depressed rounded large @click="$emit('get-started')">Let's get started!</v-btn>
 			</div>
 		</section>
 	</v-container>
@@ -101,13 +100,10 @@ export default {
 	font-family: Roboto;
 	font-style: normal;
 	font-size: 1.75em;
-	font-weight: bold;
 	display: flex;
 	align-items: center;
 	text-align: center;
-	letter-spacing: 0.06em;
-	color: #000000;
-	text-shadow: 1px 2px 5px rgba(0, 0, 0, 0.25);
+	letter-spacing: 0.04em;
 
 	h1 {
 		margin: 0 auto;
@@ -129,7 +125,6 @@ hr {
 	line-height: 21px;
 	text-align: center;
 	letter-spacing: 0.04em;
-	color: #000000;
 
 	h3 {
 		margin: 0 auto;
@@ -162,11 +157,22 @@ hr {
 }
 
 #my_social_media {
-	width: 45%;
-	display: flex;
-	justify-content: space-between;
 	margin: 0 auto;
 	padding-top: 5em;
+
+
+
+	.v-image {
+		border-radius: 50%;
+
+		&:hover {
+			box-shadow: 0px 6px 7px -4px rgba(0, 0, 0, 0.2),
+				0px 11px 15px 1px rgba(0, 0, 0, 0.14),
+				0px 4px 20px 3px rgba(0, 0, 0, 0.12) !important;
+			cursor: pointer;
+			@apply tw--mt-3;
+		}
+	}
 }
 
 #get_started {
@@ -175,7 +181,7 @@ hr {
 
 	button {
 		color: white;
-		background-color: #000000;
+		background-color: #272727;
 	}
 }
 </style>
